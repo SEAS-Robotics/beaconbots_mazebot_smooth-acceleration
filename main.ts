c5a5953885671de390cdd1f2a3a84d6c96ccfb1f
26lucab <26lucab@school.seastucson.org>
2026-04-17 11:01:52 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -849,10 +849,10 @@ quest_Note_1.quest_Show_String_For_Note_Big_Func(
 )
 turbo_mode = 0
 // The accel ramp doesn't have to start at 0. Initial testing showed that while setting any value from 0 to 100 is possible, starting speeds of over 20 caused tire slippage.  This condition can vary by terrain type.
-normal_start_speed = 20
+normal_start_speed = 30
 // This variable controls the rate of the acceleration ramp-- how long the bot takes to get from zero to max. In initial testing 1 worked well, 2 produced tire slippage.
 accel_rate = 1
-max_normal_speed = 60
+max_normal_speed = 70
 // This value will determine the maximum speed to which the bot will accelerate before topping out. Valid range is 0 to 100.
 max_turbo_speed = 99
 quest_Note_4.quest_Show_String_For_Note_Small_Func(
